1993f3471bd7813357314cc3dc8b4043d2702a2f
cy_je <ckddyd28@gmail.com>
2024-05-17 17:22:19 +0900

    for문 추가

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Git Test");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
     }
 }
